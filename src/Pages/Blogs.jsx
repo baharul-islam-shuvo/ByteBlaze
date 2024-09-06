1b3703727d0bd4daac4a30b421b6e586833eabc4
Baharul Islam Shuvo <baharulislam952@gmail.com>
2024-09-06 08:31:03 +0600

    Api fetched and blogs part done almost

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,8 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+import BlogCard from "../Components/BlogCard/BlogCard";
+
 const Blogs = () => {
+    const blogs = useLoaderData();
+    console.log(blogs);
     return (
-        <div>
-            <h2 className="text-center text-4xl mt-10 font-extrabold">This is the blogs</h2>
-        </div>
+        <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800 py-8">
+            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+                <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 dark:bg-gray-50">
+                    <img src={blogs[1].cover_image} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500 dark:bg-gray-500" />
+                    <div className="p-6 space-y-2 lg:col-span-5">
+                        <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{blogs[1].title}</h3>
+                        <span className="text-xs text-gray-400 dark:text-gray-600">{new Date(blogs[1].created_at).toLocaleDateString()}</span>
+                        <p>{blogs[1].description}</p>
+                    </div>
+                </a>
+                <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    {
+                        blogs.map(blog => <BlogCard key={blog.id} blog={blog}></BlogCard>)
+                    }
+                </div>
+            </div>
+        </section>
     );
 };
 
